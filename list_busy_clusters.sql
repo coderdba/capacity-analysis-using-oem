541b52a34c21c5c9ac26677786d93bcd011d7366
gowrishdba <gowrishdba@yahoo.com>
2015-09-20 15:01:35 +0530

    Update list_busy_clusters.sql

--- list_busy_clusters.sql
+++ list_busy_clusters.sql
@@ -2,6 +2,7 @@ set echo on
 set lines 120
 set pages 1000
 
+-- Clusters and servers (with at least one server in the cluster beyond CPU threshold in the last few days)
 spool list_busy_clusters.tsv
 
     select distinct b.composite_target_type || '|' ||  b.composite_target_name || '|' ||  a.target_name
@@ -15,6 +16,8 @@ spool list_busy_clusters.tsv
        and a.maximum > 40
        and a.target_name = b.member_target_name
   order by 3,1,2;
+  
+-- NOTE: To list only the cluster name, do a subquery and distinct of composite_target_type from that subquery
 
 spool off
 

d92b1578c537843f9be54fb325ef586973335940
gowrishdba <gowrishdba@yahoo.com>
2015-09-28 16:11:50 +0530

    Update and rename asm-disk-io-stats.sql to asm-disk-io-stats-links.txt

--- asm-disk-io-stats.sql
+++
@@ -1,3 +0,0 @@
-https://twiki.cern.ch/twiki/bin/viewfile/PSSGroup/HAandPerf?rev=1.4&filename=ASM_metadata_30012006.html
-V$ASM_DISK based on X$KFDSK, X$KFKID perform disk discovery and lists disks + usage metrics
-V$ASM_DISK_STAT based on X$KFDSK_STAT, X$KFKID List disks + usage metrics

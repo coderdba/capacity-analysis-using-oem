e98b487875ebdaf01c1f0978bd26a50be44de03d
gowrishdba <gowrishdba@yahoo.com>
2015-10-28 11:07:59 +0530

    Update and rename list_busy_cluster_servers.sql to list_busy_cluster_busy_servers.sql

--- list_busy_cluster_busy_servers.sql
+++ list_busy_cluster_busy_servers.sql
@@ -3,6 +3,9 @@ set lines 120
 set pages 1000
 
 -- Clusters and servers (with at least one server in the cluster beyond CPU threshold in the last few days)
+
+alter session set current_schema=sysman;
+
 spool list_busy_cluster_servers.tsv
 
     select distinct b.composite_target_type || '|' ||  b.composite_target_name || '|' ||  a.target_name
@@ -10,12 +13,13 @@ spool list_busy_cluster_servers.tsv
            mgmt_target_memberships b
      where b.composite_target_type = 'cluster'
        and a.target_type = 'host'
+       and b.member_target_type = 'host'
        and a.metric_name='Load'
        and a.metric_column='cpuUtil'
        and a.rollup_timestamp >= sysdate-7
        and a.maximum > 40
        and a.target_name = b.member_target_name
   order by 1;
-  
+
 spool off
 
